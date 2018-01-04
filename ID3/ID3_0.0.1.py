ded8cf7a8dcef026deff25bdb03d0edd31c0a7c9
Kevin <kliu@usau00071.attlocal.net>
2018-01-04 14:51:58 -0600

    more id3 stuff, getting initial file reading set up

--- ID3/ID3_0.0.1.py
+++ ID3/ID3_0.0.1.py
@@ -3,6 +3,15 @@ from numpy import exp, array, random, dot
 
 class ID3():
     def __init__(self):
-        
+        self.rows = 0
+        self.cols = 0
+    def readFile(self, fileName):
+        f = open(fileName, 'r')
+        arr = f.readline().split(' ')
+        self.rows = arr[0]
+        self.cols = arr[1]
+        print(self.rows)
+        print(self.cols)
 if __name__ == "__main__":
-    
+    I = ID3()
+    I.readFile('input.txt')
